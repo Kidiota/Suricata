5d3f2b4e11c2207f08eb69dc7c6d6417e7f7146b
绊智Kidiota <17718520426@163.com>
2022-06-08 00:44:13 +0800

    master

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,42 @@
 #include <Arduino.h>
 #include <Suricata_VFD.h>
+#include <iostream>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+#include <stdlib.h>
 
+using namespace std;
 
+const char *ssid1 = "Kidiota_Wifi";    //首选wifi SSID
+const char *password1 = "return01";    //首选wifi 密码
 
+WiFiUDP ntpUDP;    //更新时间
+NTPClient timeClient(ntpUDP, "ntp1.aliyun.com",8*60*60, 30*60*1000);    //设置时区
 
 void setup() {
-  /*
-  pinMode(en, OUTPUT);
-  pinMode(clk, OUTPUT);
-  pinMode(din, OUTPUT);
-  pinMode(cs, OUTPUT);
-  pinMode(Reset, OUTPUT);
-  digitalWrite(en, HIGH);
-  delayMicroseconds(100);
-  digitalWrite(Reset, LOW);
-  delayMicroseconds(5);
-  digitalWrite(Reset, HIGH);
-  VFD_init();
-  */
-  VFD_SETUP();
+  Serial.begin(115200);    //串口设置波特率
+  VFD_SETUP();    //屏幕初始化
+  WiFi.begin(ssid1, password1);    //连接WiFi
+  uint8_t i = 0;  
+  while(WiFi.status() != WL_CONNECTED){    //如果没连上WiFi，执行下列代码
+      VFD_WriteStr(i, ".");    //在屏幕上显示一个点
+      i = i + 1;    //向后移一位
+      Serial.print(".");
+      delay(100);
+    }
+  VFD_WriteStr(0,"OK");
+  Serial.print("WiFi ok");
+  delay(1000);
+  VFD_WriteStr(0,"        ");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  VFD_cmd(0xE9);// 全亮测试 屏幕
-  delay(1000);
-  
-  VFD_WriteOneChar(0, 1);
-  VFD_WriteOneChar(1, 2);
-  VFD_WriteOneChar(2, 3);
-  VFD_WriteOneChar(3, 4);
-  VFD_WriteOneChar(4, 5);
-  VFD_WriteOneChar(5, 6);
-  VFD_WriteOneChar(6, 7);
-  VFD_WriteOneChar(7, 8);
-  delay(1000);
-  
-  VFD_WriteStr(0, "ABCDEFGH");
-  delay(1000);
-
-  VFD_WriteUserFont(0,0,ziku_data[0]);//0号位字符 自定义字库中字模0
-  VFD_WriteUserFont(1,1,ziku_data[1]);
-  VFD_WriteUserFont(2,2,ziku_data[2]);
-  VFD_WriteUserFont(3,3,ziku_data[3]);
-  VFD_WriteUserFont(4,4,ziku_data[4]);
-  VFD_WriteUserFont(5,5,ziku_data[5]);
-  VFD_WriteUserFont(6,6,ziku_data[6]);
-  VFD_WriteUserFont(7,7,ziku_data[7]);
-  delay(1000);
+  timeClient.update();
+  String str=timeClient.getFormattedTime();
+  char *formattedTime=(char*)str.c_str();
+  VFD_WriteStr(0, formattedTime);
+  Serial.print(timeClient.getFormattedTime());
+  Serial.print("\n");
 }
